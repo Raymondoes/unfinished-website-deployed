0d12b2a38c65699623742b2286a17631f0af4f2a
Raymondoes <130866935+Raymondoes@users.noreply.github.com>
2025-08-16 22:26:06 +0700

    Update 404.ts

--- routes/404.ts
+++ routes/404.ts
@@ -6,7 +6,7 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, '..', 'public')));
 
 router.get('/', (req: Request, res: Response) => {
     res.render('error404');
