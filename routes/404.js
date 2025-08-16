482c3f4670859618c519af154b5a151b1801a515
Raymondoes <130866935+Raymondoes@users.noreply.github.com>
2025-08-16 22:01:05 +0700

    Update 404.js

--- routes/404.js
+++ routes/404.js
@@ -2,6 +2,10 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var express = require("express");
 var router = express.Router();
+var path = require("path");
+var app = express();
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 router.get('/', function (req, res) {
     res.render('error404');
 });
