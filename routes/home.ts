36d0d44ae16dc0ee33415733923beff23d410824
Raymondoes <130866935+Raymondoes@users.noreply.github.com>
2025-08-16 22:03:26 +0700

    Update home.ts

--- routes/home.ts
+++ routes/home.ts
@@ -1,12 +1,19 @@
 import * as express from 'express';
 import { Request, Response } from 'express';
+import * as path from 'path';
+
 const router = express.Router();
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.set('views', path.join(__dirname, '..', 'views'));
 
 router.get('/', (req: Request, res: Response) => {
   let ipAddr = req.ip;
   console.log(`Client IP: ${ipAddr}`);
-  console.log('Client:', ipAddr, 'Entered Home')
-  res.render('index');
+  console.log('Client:', ipAddr, 'Entered Home');
+  res.render('index'); 
 });
 
 module.exports = router;
