8a42a6a14d77b4f714bebcb12ac3d38452978f86
Raymondoes <130866935+Raymondoes@users.noreply.github.com>
2025-08-16 22:39:38 +0700

    Update server.ts

--- server.ts
+++ server.ts
@@ -5,7 +5,7 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(express.static(path.join(__dirname, '..', 'public')));
+app.set('views', path.join(__dirname, '..', 'views'));
 
 app.get('/', (req: Request, res: Response) => {
     res.redirect('/home');
