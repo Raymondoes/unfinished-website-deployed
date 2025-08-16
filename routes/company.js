3ce5d7011c667beab374c3f9a3d97c7350cd4cc3
Raymondoes <130866935+Raymondoes@users.noreply.github.com>
2025-08-16 22:29:09 +0700

    Update company.js

--- routes/company.js
+++ routes/company.js
@@ -5,7 +5,7 @@ var router = express.Router();
 var path = require("path");
 var app = express();
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, '..', 'public')));
 app.set('view engine', 'ejs');
 app.use(express.static('views'));
 router.get('/', function (req, res) {
