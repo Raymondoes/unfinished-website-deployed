85fb4a21c7a183c93afe3a46498211aa26a66f9d
Raymondoes <130866935+Raymondoes@users.noreply.github.com>
2025-08-16 22:23:04 +0700

    Update server.js

--- server.js
+++ server.js
@@ -3,8 +3,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var express = require("express");
 var path = require("path");
 var app = express();
+app.use(expres.static('views'));
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, '..', 'public')));
 app.get('/', function (req, res) {
     res.redirect('/home');
 });
