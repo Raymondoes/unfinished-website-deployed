affb2ba72ae5c8feb0d8fea7bcf67149704fe19a
Raymondoes <130866935+Raymondoes@users.noreply.github.com>
2025-08-16 22:03:05 +0700

    Update home.js

--- routes/home.js
+++ routes/home.js
@@ -2,6 +2,10 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var express = require("express");
 var router = express.Router();
+var path = require("path");
+var app = express();
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 router.get('/', function (req, res) {
     var ipAddr = req.ip;
     console.log("Client IP: ".concat(ipAddr));
