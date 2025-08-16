c374d59bc6867f3e1453e0c942e4ab90f3cf6943
Raymondoes <130866935+Raymondoes@users.noreply.github.com>
2025-08-16 22:02:07 +0700

    Update company.js

--- routes/company.js
+++ routes/company.js
@@ -2,7 +2,10 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var express = require("express");
 var router = express.Router();
+var path = require("path");
 var app = express();
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.static('views'));
 router.get('/', function (req, res) {
