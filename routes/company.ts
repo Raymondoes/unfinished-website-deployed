ee8c15b60b7441cd8b82e6f1e7339851c2852bf8
Raymondoes <130866935+Raymondoes@users.noreply.github.com>
2025-08-16 22:02:27 +0700

    Update company.ts

--- routes/company.ts
+++ routes/company.ts
@@ -1,8 +1,12 @@
 import * as express from 'express';
 import { Request, Response } from 'express';
 const router = express.Router();
-
+import * as path from 'path';
 const app = express();
+
+app.set('view engine', 'ejs');
+
+app.set('views', path.join(__dirname, 'views'));
 
 app.set('view engine', 'ejs');
 
