03e9e7e49dfb641bae1d4075801cae9ae1605a56
Raymondoes <130866935+Raymondoes@users.noreply.github.com>
2025-08-16 22:00:39 +0700

    Update server.ts

--- server.ts
+++ server.ts
@@ -1,10 +1,11 @@
 import * as express from 'express';
 import { Request, Response } from 'express';
+import * as path from 'path';
 const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(express.static('views'));
+app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req: Request, res: Response) => {
     res.redirect('/home');
@@ -21,5 +22,4 @@ app.use('/products', productsRouter);
 app.use('/company', companyRouter);
 app.use('/404', pagenotfounderror);
 app.use('/rainin-player', raininaudio);
-
 app.listen(9000);
