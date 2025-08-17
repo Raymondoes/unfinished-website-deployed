0f4815b74b95317873fb5a3712a8304c4ede3630
Raymondoes <130866935+Raymondoes@users.noreply.github.com>
2025-08-17 11:10:21 +0700

    Update rainin-music.ts

--- routes/rainin-music.ts
+++ routes/rainin-music.ts
@@ -6,7 +6,7 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'views')));
 
 router.get('/', (req: Request, res: Response) => {
     res.render('rainin-music');
