3c86937e5ef95b21534bce4d323c3015a1ba1074
Raymondoes <marioboygamingyt@gmail.com>
2025-09-02 21:39:19 +0700

    views updates

---
+++ views/js/locations.js
@@ -0,0 +1,17 @@
+const productsdisplay = document.getElementById('rainin-music');
+
+productsdisplay.onclick = function() {
+    location.href = '/rainin-player';
+}
+
+function home() {
+    location.href = '/home';
+}
+
+function products() {
+    location.href = '/products';
+}
+
+function company() {
+    location.href = '/company';
+}
